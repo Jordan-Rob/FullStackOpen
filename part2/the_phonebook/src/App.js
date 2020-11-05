76f8789f6c87d864c8af8f46ed22a86ddfd15f70
jordanrob709 <jordanrob709@gmail.com>
2020-11-05 17:13:48 +0300

    Exercise 2.8: The Phonebook Step3

---
+++ part2/the_phonebook/src/App.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+
+const App = () => {
+    const [ persons, setPersons ] = useState([
+      { name: 'Arto Hellas', id:0 }
+    ]) 
+    const [ newName, setNewName ] = useState('')
+    const [ newNum, setNewNum] = useState('')
+  
+    const addPerson = (event) => {
+      event.preventDefault()
+  
+      const existent = persons.find(person => person.name === newName)
+  
+      if (existent){
+        window.alert(`${newName} already exists`)
+      }else {
+      const newObj = {
+        name:newName,
+        phone:newNum,
+        date: new Date().toISOString(),
+        id: persons.length + 1
+        }
+  
+        setPersons(persons.concat(newObj))
+        setNewName('')
+        setNewNum('')
+      }
+      
+    }
+  
+    const inputChange = (event) => {
+      console.log(event.target.value)
+      setNewName(event.target.value)
+    }
+
+    const numChange = (event) => {
+        console.log(event.target.value)
+        setNewNum(event.target.value)
+      }
+  
+  
+    return (
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+            name: <input value={newName} onChange={inputChange} />
+            number: <input value={newNum} onChange={numChange} />
+            <button type="submit">add</button>
+        </form>
+        <h2>Numbers</h2>
+        {persons.map( person => <p key={person.id}> {person.name} - {person.phone} </p> )}
+      </div>
+    )
+  }
+
+export default App;
--- part2/the_phonebook/src/index.js
+++ part2/the_phonebook/src/index.js
@@ -1,50 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', id:0 }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-
-  const addPerson = (event) => {
-    event.preventDefault()
-
-    const existent = persons.find(person => person.name === newName)
-
-    if (existent){
-      window.alert(`${newName} already exists`)
-    }else {
-    const newObj = {
-      name:newName,
-      date: new Date().toISOString(),
-      id: persons.length + 1
-      }
-
-      setPersons(persons.concat(newObj))
-      setNewName('')
-    }
-    
-  }
-
-  const inputChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
-
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-          name: <input value={newName} onChange={inputChange} />
-          <button type="submit">add</button>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map( person => <p key={person.id}> {person.name} </p> )}
-    </div>
-  )
-}
 
 ReactDOM.render(
   <React.StrictMode>
